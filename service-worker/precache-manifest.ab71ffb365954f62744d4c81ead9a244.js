9452f6d2b1aa8f4cce9bd52c1fe8b4e4c4c4a8a8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-19 16:11:10 +0000

    Deploy Mister-Hope/vue-template to github.com/Mister-Hope/vue-template.git:gh-pages

---
+++ service-worker/precache-manifest.ab71ffb365954f62744d4c81ead9a244.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27ccd7433ee3f1a1d17b",
+    "url": "/css/about.b68693e2.css"
+  },
+  {
+    "revision": "1bbf14e2eb800d07e88a",
+    "url": "/css/app.36d49c56.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "49059bff3f72d0b327de344d18fdffdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27ccd7433ee3f1a1d17b",
+    "url": "/js/about.f6631300.js"
+  },
+  {
+    "revision": "1bbf14e2eb800d07e88a",
+    "url": "/js/app.4506fbf8.js"
+  },
+  {
+    "revision": "d2a601fbf117de1da0a4",
+    "url": "/js/manifest.86386d55.js"
+  },
+  {
+    "revision": "1cf82a8948f8d2704f1461ebadde224f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ service-worker/precache-manifest.b5686abcc8fedaaac69b4ec00495c453.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27ccd7433ee3f1a1d17b",
+    "url": "/css/about.b68693e2.css"
+  },
+  {
+    "revision": "d91ff51cfe15d1686861",
+    "url": "/css/app.36d49c56.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b08d9728c7597a4fb5ac4ae02bd4fc7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27ccd7433ee3f1a1d17b",
+    "url": "/js/about-legacy.f6631300.js"
+  },
+  {
+    "revision": "d91ff51cfe15d1686861",
+    "url": "/js/app-legacy.f76e07d1.js"
+  },
+  {
+    "revision": "fe35c9846365e52b197c",
+    "url": "/js/manifest-legacy.ab9d6c77.js"
+  },
+  {
+    "revision": "1cf82a8948f8d2704f1461ebadde224f",
+    "url": "/manifest.json"
+  }
+]);
