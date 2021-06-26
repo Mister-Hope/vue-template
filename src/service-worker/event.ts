9529d61133c46482ca7a2a3052bd185d014a163a
Mr.Hope <zhangbowang1998@gmail.com>
2021-06-26 18:22:38 +0800

    feat: update deps

--- src/service-worker/event.ts
+++ src/service-worker/event.ts
@@ -1,25 +1,15 @@
 import mitt from "mitt";
 
-import type { Emitter, Handler, WildcardHandler } from "mitt";
+import type { Emitter } from "mitt";
 
-export interface PwaEvent extends Emitter {
-  on(type: "ready", handler: Handler<ServiceWorkerRegistration>): void;
-  on(type: "registered", handler: Handler<ServiceWorkerRegistration>): void;
-  on(type: "cached", handler: Handler<ServiceWorkerRegistration>): void;
-  on(type: "updatefound", handler: Handler<ServiceWorkerRegistration>): void;
-  on(type: "updated", handler: Handler<ServiceWorkerRegistration>): void;
-  on(type: "offline", handler: Handler<void>): void;
-  on(type: "error", handler: Handler<Error>): void;
-  on(type: "*", handler: WildcardHandler): void;
-  emit(type: "ready", event: ServiceWorkerRegistration): void;
-  emit(type: "registered", event: ServiceWorkerRegistration): void;
-  emit(type: "cached", event: ServiceWorkerRegistration): void;
-  emit(type: "updatefound", event: ServiceWorkerRegistration): void;
-  emit(type: "updated", event: ServiceWorkerRegistration): void;
-  emit(type: "offline"): void;
-  emit(type: "error", event: Error): void;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  emit(type: "*", event?: any): void;
-}
+export type PwaEvent = Emitter<{
+  ready: ServiceWorkerRegistration;
+  registered: ServiceWorkerRegistration;
+  cached: ServiceWorkerRegistration;
+  updatefound: ServiceWorkerRegistration;
+  updated: ServiceWorkerRegistration;
+  offline: void;
+  error: Error;
+}>;
 
 export const pwaEvent: PwaEvent = mitt();
